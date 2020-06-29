3dcf88d23f915a863d3d881af8bb957531b5d83a
nagase123 <nagase0926@gmail.com>
2020-06-29 09:52:12 +0900

    learn props of component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
 
-function App() {
+const App = () => {
+  const profiles = [
+    {
+      name: 'Tanaka',
+      age: 12,
+    },
+    {
+      name: 'Sato',
+      age: 39,
+    },
+    {
+      name: 'NoName',
+    },
+  ]
   return (
     <div>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />
+      })}
       <h1>Hello, world</h1>
     </div>
   )
+}
+
+const User = (props: { name: string; age: number }) => {
+  return (
+    <div>
+      I am {props.name}! And {props.age} years old.
+    </div>
+  )
+}
+User.defaultProps = {
+  age: 1,
 }
 
 export default App
